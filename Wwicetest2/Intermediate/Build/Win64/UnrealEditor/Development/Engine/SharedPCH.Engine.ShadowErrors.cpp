d57a03a8c9936d12fe16cd3b759a1b752b6373a7
Robert Printa Larsson <djprinta@gmail.com>
2022-11-11 12:47:11 +0100

    Flyttade Wwice Projektet utan för repon

--- Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30145
-#include "C:/Git/WWise-intergaration-test/Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30145
-#include "C:/Git/WWise-intergaration-test/Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
