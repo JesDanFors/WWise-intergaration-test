809dec76f77ca42fa2c753d1c038b4853f07a78d
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-11-11 13:53:20 +0100

    working plugin

---
+++ Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "C:/Forsbergsskola/Unreal/WWise-intergaration-test/Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "C:/Forsbergsskola/Unreal/WWise-intergaration-test/Wwicetest2/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
